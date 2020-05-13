dda9e4622c026cbe99eb819500e995ca9f378fef
MrSRK <rubes6@hotmail.com>
2020-05-13 16:01:36 +0300

    env

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 "use strict"
-//const dotenv=require('dotenv')
-//dotenv.config()
+const dotenv=require('dotenv')
+dotenv.config()
 var CJS=require('./src/cjs')
 var config=require('./config.json')
 const app=new CJS(config)
